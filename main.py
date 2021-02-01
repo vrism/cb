d301158bd023014f5bc6a5e69dd794bc3cafa419
vrism <vrism@vrismui-MacBookPro.local>
2021-02-01 18:31:30 +0900

    first commit

--- main.py
+++ main.py
@@ -3,14 +3,30 @@
 # Press ⇧F10 to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QLineEdit, QPushButton, QFileDialog, QDesktopWidget
+import os
+import shbackup
+import json
+import threading
+from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout, QLabel, QLineEdit, QPushButton, QFileDialog, \
+	QDesktopWidget, QMessageBox
+
+
+RECENT_DIR_JSON = 'recent_dir.json'
+
+
+class RecentDirJson:
+	SRC = "src"
+	RAW = "raw"
+	ETC = "etc"
 
 
 class MyApp(QWidget):
 
+	is_job_running = False
+	t = None
+
 	def __init__(self):
 		super().__init__()
-		self.edit_src = QLineEdit()
 
 		grid = QGridLayout()
 		self.setLayout(grid)
@@ -18,40 +34,79 @@ class MyApp(QWidget):
 		grid.addWidget(QLabel('Source'), 0, 0)
 		grid.addWidget(QLabel('For Raw'), 1, 0)
 		grid.addWidget(QLabel('For Etc'), 2, 0)
-		grid.addWidget(QLabel('message'), 4, 0)
+		grid.addWidget(QLabel(), 4, 0)
 
-		self.edit_src = QLineEdit()
-		self.edit_raw = QLineEdit()
-		self.edit_etc = QLineEdit()
+		# get/set recent used directory
+		with open(RECENT_DIR_JSON, 'r') as cf:
+			self.recent = json.load(cf)
+			self.recent_src = self.recent[RecentDirJson.SRC]
+			self.recent_raw = self.recent[RecentDirJson.RAW]
+			self.recent_etc = self.recent[RecentDirJson.ETC]
+
+		if self.recent_src:
+			self.edit_src = QLineEdit(self.recent_src)
+		else:
+			self.edit_src = QLineEdit(os.getcwd())
+
+		if self.recent_raw:
+			self.edit_raw = QLineEdit(self.recent_raw)
+		else:
+			self.edit_raw = QLineEdit(os.getcwd())
+
+		if self.recent_etc:
+			self.edit_etc = QLineEdit(self.recent_etc)
+		else:
+			self.edit_etc = QLineEdit(os.getcwd())
+
 		grid.addWidget(self.edit_src, 0, 1)
 		grid.addWidget(self.edit_raw, 1, 1)
 		grid.addWidget(self.edit_etc, 2, 1)
 
 		self.btn_start = QPushButton('Start')
+		self.btn_start.clicked.connect(self.btn_start_clicked)
 		self.btn_stop = QPushButton('Stop')
+		self.btn_stop.clicked.connect(self.btn_stop_clicked)
 		grid_inner = QGridLayout()
 		grid_inner.addWidget(self.btn_start, 0, 0)
 		grid_inner.addWidget(self.btn_stop, 0, 1)
 		grid.addLayout(grid_inner, 3, 1)
+		self.btn_start.setEnabled(True)
+		self.btn_stop.setEnabled(False)
 
-		self.lbl_message = QLabel()
+		self.lbl_message = QLabel('')
 		grid.addWidget(self.lbl_message, 4, 1)
 
 		self.btn_src = QPushButton('...')
-		self.btn_src.clicked.connect(self.btn_src_clicked)
 		self.btn_raw = QPushButton('...')
-		self.btn_raw.clicked.connect(self.btn_raw_clicked)
 		self.btn_etc = QPushButton('...')
+		self.btn_src.clicked.connect(self.btn_src_clicked)
+		self.btn_raw.clicked.connect(self.btn_raw_clicked)
 		self.btn_etc.clicked.connect(self.btn_etc_clicked)
 		grid.addWidget(self.btn_src, 0, 2)
 		grid.addWidget(self.btn_raw, 1, 2)
 		grid.addWidget(self.btn_etc, 2, 2)
 
-		self.setWindowTitle('Vrism Content Backup')
+		self.setWindowTitle('Vrism Content Backup Tool')
 		# self.move(300, 300)
 		self.center()
-		self.resize(800, 400)
+		self.resize(600, 200)
 		self.show()
+
+	def closeEvent(self, event) -> None:
+		if self.is_job_running:
+			reply = QMessageBox.question(self,
+										 'Message',
+					    			     'now on you task, Are sure to Quit?',
+										 QMessageBox.Yes | QMessageBox.No,
+										 QMessageBox.No)
+			if reply == QMessageBox.Yes:
+				event.accept()
+				shbackup.stop = True
+				self.t.join()
+			else:
+				event.ignore()
+		else:
+			event.accept()
 
 	def center(self):
 		qr = self.frameGeometry()
@@ -59,14 +114,52 @@ class MyApp(QWidget):
 		qr.moveCenter(cp)
 		self.move(qr.topLeft())
 
+	def set_status(self, msg, running = True):
+		self.lbl_message.setText(msg)
+		self.is_job_running = running
+		if running:
+			self.btn_start.setEnabled(False)
+			self.btn_stop.setEnabled(True)
+		else:
+			self.btn_start.setEnabled(True)
+			self.btn_stop.setEnabled(False)
+
+	def btn_start_clicked(self):
+		if self.is_job_running:
+			return
+		self.t = threading.Thread(target = shbackup.run,
+							 args = (self.edit_src.text(),
+									 self.edit_raw.text(),
+									 self.edit_etc.text(),
+									 self.set_status))
+		self.t.start()
+		with open(RECENT_DIR_JSON, 'w') as cf:
+			self.recent[RecentDirJson.SRC] = self.edit_src.text()
+			self.recent[RecentDirJson.RAW] = self.edit_raw.text()
+			self.recent[RecentDirJson.ETC] = self.edit_etc.text()
+			json.dump(self.recent, cf, indent = 4)
+
+	def btn_stop_clicked(self):
+		if not self.is_job_running:
+			return
+		shbackup.stop = True
+		self.t.join()
+		self.t = None
+
 	def btn_src_clicked(self):
-		self.edit_src.setText(self.show_file_dialog())
+		sel = self.show_file_dialog()
+		if sel:
+			self.edit_src.setText(sel)
 
 	def btn_raw_clicked(self):
-		self.edit_raw.setText(self.show_file_dialog())
+		sel = self.show_file_dialog()
+		if sel:
+			self.edit_raw.setText(sel)
 
 	def btn_etc_clicked(self):
-		self.edit_etc.setText(self.show_file_dialog())
+		sel = self.show_file_dialog()
+		if sel:
+			self.edit_etc.setText(sel)
 
 	def show_file_dialog(self):
 		return QFileDialog.getExistingDirectory(self, 'Select a folder', '.')
--- shbackup.py
+++ shbackup.py
@@ -1,55 +1,116 @@
 import shutil
 import os
+import logging
+import json
+import threading
+import time
 
-list_big = ['raw']
-list_work = ['blender', 'modeling', 'texture', 'motion']
-list_delete = ['jpg', 'jpgm']
+# list_big = ['raw']
+# list_etc = ['blender', 'modeling', 'texture', 'motion']
+# list_del = ['jpg', 'jpgm']
+
+CONFIG_JSON = 'config.json'
 
 
-def copy_in_dir(src, dst):
+class ConfigJson:
+	BIG = "big"
+	ETC = "etc"
+	DEL = "del"
+
+
+stop = False
+
+
+def copy_content(src, dst, func):
+	global stop
+	i = 1
+	num_files = sum(1 for item in os.listdir(src) if os.path.isfile(os.path.join(src, item)))
 	for f in os.listdir(src):
-		shutil.copy2(f, dst)
+		if stop:
+			break
+		src_file = os.path.join(src, f)
+		if not os.path.isfile(src_file):
+			continue
+		shutil.copy2(src_file, dst)
+		if func is not None:
+			msg = '(%d/%d) files in %s transmitted.' % (i, num_files, src)
+			func(msg)
+			i += 1
 
 
-def backup(src, dst_big_root, dst_work_root):
+def validate_dir(file, src, dst):
+	src_dir = os.path.join(src, file)
+	dst_dir = os.path.join(dst, file)
+	if not os.path.exists(dst_dir):
+		os.mkdir(dst_dir)
+	return src_dir, dst_dir
+
+
+def backup(src, dst_big, dst_etc, func):
+	cf = open(CONFIG_JSON, 'r')
+	config = json.load(cf)
+	list_big = config[ConfigJson.BIG]
+	list_etc = config[ConfigJson.ETC]
+	list_del = config[ConfigJson.DEL]
+
+	global stop
 	for f in os.listdir(src):
+		if stop:
+			break
 		fl = f.lower()
-		if fl in list_delete:
-			print('del : ' + f)
-		elif fl in list_big:
-			print('big : ' + f)
-			src_dir = os.path.join(src, fl)
-			dst_dir = os.path.join(dst_big_root, fl)
-			print(src_dir)
-			print(dst_dir)
-			os.mkdir(dst_dir)
-			copy_in_dir(src_dir, dst_dir)
-		elif fl in list_work:
-			print('work : ' + f)
-			src_dir = os.path.join(src, fl)
-			dst_dir = os.path.join(dst_work_root, fl)
-			print(src_dir)
-			print(dst_dir)
-			os.mkdir(dst_dir)
-			copy_in_dir(src_dir, dst_dir)
+		if fl in list_big:
+			src_dir, dst_dir = validate_dir(fl, src, dst_big)
+			copy_content(src_dir, dst_dir, func)
+		elif fl in list_etc:
+			src_dir, dst_dir = validate_dir(fl, src, dst_etc)
+			copy_content(src_dir, dst_dir, func)
+		elif fl in list_del:
+			# todo
+			continue
 		else:
 			continue
 
 
-def run(src, dst_big, dst_work):
-	for f in os.listdir(src):
-		cid_path = os.path.join(src, f)  					# src_root/content_id
-		if os.path.isdir(cid_path):
-			dst_big_c_id = os.path.join(dst_big, f)			# raw/content_id
-			dst_work_c_id = os.path.join(dst_work, f)		# work/content_id
-			if not os.path.exists(dst_big_c_id):
-				os.mkdir(dst_big_c_id)
-			if not os.path.exists(dst_work_c_id):
-				os.mkdir(dst_work_c_id)
+def run(src_root, dst_big, dst_etc, callback):
+	global stop
 
-			backup(cid_path, dst_big_c_id, dst_work_c_id)
+	if not os.path.exists(src_root):
+		return
+	if not os.path.exists(dst_big):
+		return
+	if not os.path.exists(dst_etc):
+		return
 
+	callback("Starting...")
+	for i in range(3):
+		if stop:
+			break
+		callback(str(i + 1))
+		time.sleep(1)
+	stop = False
+	callback("Done...", False)
+	return
 
-run('/Users/vrism/work/tester/python/shell_util/sh_src',
-	'/Users/vrism/work/tester/python/shell_util/sh_dst_raw',
-	'/Users/vrism/work/tester/python/shell_util/sh_dst_work')
+	logger = logging.getLogger()
+
+	try:
+		for file in os.listdir(src_root):
+			if stop:
+				break
+			src_cid = os.path.join(src_root, file)				# src_root/{content_id}
+			if os.path.isdir(src_cid):
+				dst_big_cid = os.path.join(dst_big, file)		# dst_big/{content_id}
+				dst_etc_cid = os.path.join(dst_etc, file)		# dst_etc/{content_id}
+
+				if not os.path.exists(dst_big_cid):
+					os.mkdir(dst_big_cid)
+				if not os.path.exists(dst_etc_cid):
+					os.mkdir(dst_etc_cid)
+
+				backup(src_cid, dst_big_cid, dst_etc_cid, callback)
+	except Exception as e:
+		logger.exception(e)
+		print(e)
+
+	stop = False
+	callback('Done.', False)
